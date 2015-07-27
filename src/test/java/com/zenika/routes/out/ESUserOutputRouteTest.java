99230bc6ae4d2d64dfe3a23038db3b0c55e2b39b
agouriou <armel.gouriou@gmail.com>
2015-07-27 12:32:08 +0200

    création index pour test

--- src/test/java/com/zenika/routes/out/ESUserOutputRouteTest.java
+++ src/test/java/com/zenika/routes/out/ESUserOutputRouteTest.java
@@ -4,7 +4,15 @@ import com.zenika.model.user.User;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.DirtiesContext;
@@ -23,8 +31,35 @@ public class ESUserOutputRouteTest {
     @Produce(uri = "direct:esUsersOutput")
     private ProducerTemplate esUserOutput;
 
-    @Produce(uri = "elasticsearch://local?operation=GET_BY_ID&indexName=user&indexType=user")
+    @Produce(uri = "elasticsearch://elasticsearch?ip=127.0.0.1&operation=GET_BY_ID&indexName=user&indexType=user")
     private ProducerTemplate esQueryUser;
+
+//    @BeforeClass
+//    public static void setup(){
+//        Node node = NodeBuilder.nodeBuilder().client(true).clusterName("elasticsearch")
+//                .node();
+//        Client client = node.client();
+//        try {
+//            client.admin().indices().create(Requests.createIndexRequest("user")).actionGet();
+//        }catch (IndexAlreadyExistsException e){
+//            //ok, index already created
+//            System.out.println("############################################# EXISTING");
+//        }
+//    }
+
+    @BeforeClass
+    public static void setup(){
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("cluster.name", "elasticsearch").build();
+        Client client = new TransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+        try {
+            client.admin().indices().create(Requests.createIndexRequest("user")).actionGet();
+        }catch (IndexAlreadyExistsException e){
+            //ok, index already created
+            System.out.println("############################################# EXISTING");
+        }
+    }
 
     @Test
     public void should_index_1_user(){
@@ -37,5 +72,8 @@ public class ESUserOutputRouteTest {
 
         Object userFromEs = esQueryUser.requestBody(index);
         Assert.assertNotNull(userFromEs);
+
+
+
     }
 }
